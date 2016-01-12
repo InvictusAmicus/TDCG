594ad1f68a61a39008f7ed98e9c69a69b4e336b6
CiaranClerkin <K00191500@student.lit.ie>
2016-01-12 14:59:56 +0000

    UI template finished, Functionality needed

--- Options.cpp
+++ Options.cpp
@@ -51,18 +51,40 @@ bool Options::init()
 	this->addChild(sprite, 0);
 
 	
-	auto checkBox = cocos2d::ui::CheckBox::create("check_box_normal.png",
+	auto MusicCheckBox = cocos2d::ui::CheckBox::create("check_box_normal.png",
 		"check_box_normal_press.png",
 		"check_box_active.png",
 		"check_box_normal_disable.png",
 		"check_box_active_disable.png");
-	checkBox->setPosition(Vec2(origin.x + visibleSize.width / 2,
-		origin.y + visibleSize.height - checkBox->getContentSize().height * 14));
+	MusicCheckBox->setPosition(Vec2(origin.x + visibleSize.width / 2,
+		origin.y + visibleSize.height - MusicCheckBox->getContentSize().height * 12));
 	//checkBox->addEventListener(CC_CALLBACK_2(UICheckBoxTest::selectedEvent, this));
-	this->addChild(checkBox,1);
+	this->addChild(MusicCheckBox,1);
+
+	auto MusicCheckBoxLabel = Label::createWithTTF("Mute Music", "fonts/Marker Felt.ttf", 24);
+	MusicCheckBoxLabel->setColor(ccc3(0, 0, 0));
+	MusicCheckBoxLabel->setPosition(Vec2((origin.x + visibleSize.width / 2) - 250,
+		origin.y + visibleSize.height - MusicCheckBox->getContentSize().height * 12));
+	this->addChild(MusicCheckBoxLabel, 1);
+
+	auto SoundEffectCheckBox = cocos2d::ui::CheckBox::create("check_box_normal.png",
+		"check_box_normal_press.png",
+		"check_box_active.png",
+		"check_box_normal_disable.png",
+		"check_box_active_disable.png");
+	SoundEffectCheckBox->setPosition(Vec2(origin.x + visibleSize.width / 2,
+		origin.y + visibleSize.height - SoundEffectCheckBox->getContentSize().height * 14));
+	//checkBox->addEventListener(CC_CALLBACK_2(UICheckBoxTest::selectedEvent, this));
+	this->addChild(SoundEffectCheckBox, 1);
+
+	auto SoundEffectCheckBoxLabel = Label::createWithTTF("Mute Sound Effect", "fonts/Marker Felt.ttf", 24);
+	SoundEffectCheckBoxLabel->setColor(ccc3(0, 0, 0));
+	SoundEffectCheckBoxLabel->setPosition(Vec2((origin.x + visibleSize.width / 2) - 250,
+		origin.y + visibleSize.height - SoundEffectCheckBox->getContentSize().height * 14));
+	this->addChild(SoundEffectCheckBoxLabel, 1);
 
 
-	//auto b = cocos2d::ui::CheckBox::create();
+
 	auto MusicSlider = cocos2d::ui::Slider::create();
 	MusicSlider->loadBarTexture("sliderTrack.png");
 	MusicSlider->loadSlidBallTextures("sliderThumb.png", "sliderThumb.png", "");
@@ -71,15 +93,29 @@ bool Options::init()
 		origin.y + visibleSize.height - MusicSlider->getContentSize().height * 4));
 	//MusicSlider->addEventListener(CC_CALLBACK_2(UISliderTest::sliderEvent, this));
 	this->addChild(MusicSlider,1);
+
+	auto MusicLabel = Label::createWithTTF("Music Volume", "fonts/Marker Felt.ttf", 24);
+	MusicLabel->setColor(ccc3(0,0,0));
+	MusicLabel->setPosition(Vec2((origin.x + visibleSize.width / 2) - 250,
+		origin.y + visibleSize.height - MusicSlider->getContentSize().height * 4));
+	this->addChild(MusicLabel, 1);
+	
 	
 	auto SoundEffectSlider = cocos2d::ui::Slider::create();
 	SoundEffectSlider->loadBarTexture("sliderTrack.png");
 	SoundEffectSlider->loadSlidBallTextures("sliderThumb.png", "sliderThumb.png", "");
 	SoundEffectSlider->loadProgressBarTexture("sliderProgress.png");
 	SoundEffectSlider->setPosition(Vec2(origin.x + visibleSize.width / 2,
-		origin.y + visibleSize.height - MusicSlider->getContentSize().height * 6));
+		origin.y + visibleSize.height - SoundEffectSlider->getContentSize().height * 6));
 	//SoundEffectSlider->addEventListener(CC_CALLBACK_2(UISliderTest::sliderEvent, this));
 	this->addChild(SoundEffectSlider,1);
+
+	auto SoundEffectLabel = Label::createWithTTF("Sound Effect Volume", "fonts/Marker Felt.ttf", 24);
+	SoundEffectLabel->setColor(ccc3(0, 0, 0));
+	SoundEffectLabel->setPosition(Vec2((origin.x + visibleSize.width / 2) - 250,
+		origin.y + visibleSize.height - SoundEffectSlider->getContentSize().height * 6));
+	this->addChild(SoundEffectLabel, 1);
+
 
 	//Original Back to menu
 	//auto BackToMain = MenuItemImage::create("Options.png", "Continue.png", CC_CALLBACK_1(Options::menuReturn, this));
