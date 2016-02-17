e98da71d1eaf64e5c431b550be9bd89afcc0582f
CiaranClerkin <K00191500@student.lit.ie>
2016-02-17 21:13:14 +0000

    Changes card lore based on card

--- Card.cpp
+++ Card.cpp
@@ -18,6 +18,87 @@ Card::Card(std::string s)
 	cardSprite = cocos2d::Sprite::create(s);
 	name = s;
 	lore = "Unique Lore";
+	
+	/*
+	Remove the comments relating to the cards when they have the appropriate name and lore assigned
+	*/
+	
+	//Card Name to be changed later
+	if (name == "SampleCard.png")
+	{
+		//Remove when proper lore is added
+		lore = "SampleCard.png";
+	}
+
+	//Card Name to be changed later
+	else if (name == "SampleCard2.png")
+	{
+		//Remove when proper lore is added
+		lore = "SampleCard2.png";
+	}
+
+	//Card Name to be changed later
+	else if (name == "Archfiend.png")
+	{
+		//Remove when proper lore is added
+		lore = "Archfiend.png";
+	}
+
+	//Card Name to be changed later
+	else if (name == "A.I.P.png")
+	{
+		//Remove when proper lore is added
+		lore = "A.I.P.png";
+	}
+
+	//Card Name to be changed later
+	else if (name == "Aviater.png")
+	{
+		//Remove when proper lore is added
+		lore = "Aviater.png";
+	}
+
+	//Card Name to be changed later
+	else if (name == "Cain.png")
+	{
+		//Remove when proper lore is added
+		lore = "Cain.png";
+	}
+
+	//Card Name to be changed later
+	else if (name == "Indicted.png")
+	{
+		//Remove when proper lore is added
+		lore = "Indicted.png";
+	}
+
+	//Card Name to be changed later
+	else if (name == "MoltonCore.png")
+	{
+		//Remove when proper lore is added
+		lore = "MoltonCore.png";
+	}
+
+	//Card Name to be changed later
+	else if (name == "IceDemon.png")
+	{
+		//Remove when proper lore is added
+		lore = "IceDemon.png";
+	}
+
+	//Card Name to be changed later
+	else if (name == "Tariq.png")
+	{
+		//Remove when proper lore is added
+		lore = "Tariq.png";
+	}
+
+	//Card Name to be changed later
+	else if (name == "Wisp.png")
+	{
+		//Remove when proper lore is added
+		lore = "Wisp.png";
+	}
 }
 
 Card::~Card()
